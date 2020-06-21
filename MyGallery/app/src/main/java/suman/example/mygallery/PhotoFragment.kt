61709ed990f206c32fff9eb5b796cb082e3d33ae
voltovo87 <voltovo87@gmail.com>
2020-06-21 23:38:58 +0900

    refactor MyGallery Dev ing...

--- MyGallery/app/src/main/java/suman/example/mygallery/PhotoFragment.kt
+++ MyGallery/app/src/main/java/suman/example/mygallery/PhotoFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_photo.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+//const를 사용하여 시간에 결정되는 상수 선언
+//파일 내 어디서든지 사용 가능
+private const val ARG_URI = "uri"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,14 +21,12 @@ private const val ARG_PARAM2 = "param2"
  */
 class PhotoFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var uri: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            uri = it.getString(ARG_URI)
         }
     }
 
@@ -33,26 +34,25 @@ class PhotoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        //onCreateView()로 프래그먼테 표시될 뷰 생성
+        //액티비티가 아닌곳에서 레이아웃 리소스를 가져오기 위해서 inflate() 사용
         return inflater.inflate(R.layout.fragment_photo, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //이미지가 로딩되면 into() 메서드로 imageView에 표사
+        Glide.with(this).load(uri).into(imageView)
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PhotoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        //newInstance()이용해서 프래그먼트를 생성, 인자로 uri를 전달
+        fun newInstance(uri: String) =
             PhotoFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(ARG_URI,uri)
                 }
             }
     }
