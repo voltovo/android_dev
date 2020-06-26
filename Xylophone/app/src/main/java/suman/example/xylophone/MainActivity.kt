f98c849fd629742bce37856fbda04d5016d16255
voltovo87 <voltovo87@gmail.com>
2020-06-26 22:30:52 +0900

    refactor Xylophone Dev ing...

--- Xylophone/app/src/main/java/suman/example/xylophone/MainActivity.kt
+++ Xylophone/app/src/main/java/suman/example/xylophone/MainActivity.kt
@@ -1,10 +1,13 @@
 package suman.example.xylophone
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //화면이 가로 모드로 고정되게 하기
+        //requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
